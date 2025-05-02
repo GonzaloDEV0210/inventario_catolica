5b084894907b4f3ed7e9ebb26ee785eb75de1901
Gonzalo Palma <133717685+GonzaloDEV0210@users.noreply.github.com>
2025-05-01 21:02:07 -0500

    icono subido a la pestaña

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hola mundo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Inventario - Católica School</title>
+    <link rel="shortcut icon" href="./assets/icons/icon.png" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
