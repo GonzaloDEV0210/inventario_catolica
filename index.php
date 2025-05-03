3c835ecac40bd7e524c6006b5b4bbfaf10a9ba69
Gonzalo_Palma <gonzalopalmadev0210@gmail.com>
2025-05-03 17:06:08 -0500

    empezando

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Inventario - Católica School</title>
     <link rel="shortcut icon" href="./assets/icons/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/styles.css">
 </head>
 <body>
-    
+    <header>
+        <img src="./assets/img/logo.png" alt="logo institucional">
+    </header>
+    <main></main>
+    <footer></footer>
 </body>
 </html>
